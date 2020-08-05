a385a0154f0a5186c8f1a16e890a7b3e776d14cc
Xuzhao Gao <xuzhao.gao@IntelligentDataSystems.local>
2020-08-05 16:34:47 -0400

    POW-1200

--- www/js/AppConfig.js
+++ www/js/AppConfig.js
@@ -6,9 +6,9 @@ window.FMSApplication.constant('appConfig',
         baseApiUrl: 'https://fmportal.powerhouseretail.com/MobileTraining/api/',
         // baseApiUrl: 'http://fmsync.intelligentdata.com/WebAPI/api/',
         baseErrorLogUrl: 'http://fmsync.intelligentdata.com/ErrorLogging/api/',
-        baseDocumentUrl: 'https://fmportal.powerhouseretail.com/MobileNew/Documents/Download/',
-        baseDocumentWithoutRedirectUrl: 'http://fmsync.intelligentdata.com/WebAPI/Documents/DownloadWithoutRedirect/',
-        baseThumbnailDocumentUrl: 'https://fmportal.powerhouseretail.com/MobileNew/Documents/Thumbnail/',
+        baseDocumentUrl: 'https://fmportal.powerhouseretail.com/MobileTraining/Documents/Download/',
+        baseDocumentWithoutRedirectUrl: 'http://fmsync.intelligentdata.com/MobileTraining/Documents/DownloadWithoutRedirect/',
+        baseThumbnailDocumentUrl: 'https://fmportal.powerhouseretail.com/MobileTraining/Documents/Thumbnail/',
         databaseName: 'FMSync.DB',
         databaseVersion: '1.0'
 
