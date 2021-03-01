b1e568e498479562d7147bd718db2eaac740e408
Xuzhao Gao <Xuzhao@facilit.fm>
2021-03-01 12:11:17 -0500

    POW-1414

--- www/js/AppConfig.js
+++ www/js/AppConfig.js
@@ -1,22 +1,22 @@
 window.FMSApplication.constant('appConfig',
     {
         //ph live
-        // baseApiUrl: 'https://fmportal.powerhouseretail.com/MobileNew/api/',
-        // baseErrorLogUrl: 'http://fmsync.intelligentdata.com/ErrorLogging/api/',
-        // baseDocumentUrl: 'https://fmportal.powerhouseretail.com/MobileNew/Documents/Download/',
-        // baseDocumentWithoutRedirectUrl: 'http://fmsync.intelligentdata.com/MobileNew/Documents/DownloadWithoutRedirect/',
-        // baseThumbnailDocumentUrl: 'https://fmportal.powerhouseretail.com/MobileNew/Documents/Thumbnail/',
-        // databaseName: 'FMSync.DB',
-        // databaseVersion: '1.0'
-
-        //PH training
-        baseApiUrl: 'https://fmportal.powerhouseretail.com/MobileTraining/api/',
+        baseApiUrl: 'https://fmportal.powerhouseretail.com/MobileNew/api/',
         baseErrorLogUrl: 'http://fmsync.intelligentdata.com/ErrorLogging/api/',
-        baseDocumentUrl: 'https://fmportal.powerhouseretail.com/MobileTraining/Documents/Download/',
-        baseDocumentWithoutRedirectUrl: 'http://fmsync.intelligentdata.com/MobileTraining/Documents/DownloadWithoutRedirect/',
-        baseThumbnailDocumentUrl: 'https://fmportal.powerhouseretail.com/MobileTraining/Documents/Thumbnail/',
+        baseDocumentUrl: 'https://fmportal.powerhouseretail.com/MobileNew/Documents/Download/',
+        baseDocumentWithoutRedirectUrl: 'http://fmsync.intelligentdata.com/MobileNew/Documents/DownloadWithoutRedirect/',
+        baseThumbnailDocumentUrl: 'https://fmportal.powerhouseretail.com/MobileNew/Documents/Thumbnail/',
         databaseName: 'FMSync.DB',
         databaseVersion: '1.0'
+
+        //PH training
+        // baseApiUrl: 'https://fmportal.powerhouseretail.com/MobileTraining/api/',
+        // baseErrorLogUrl: 'http://fmsync.intelligentdata.com/ErrorLogging/api/',
+        // baseDocumentUrl: 'https://fmportal.powerhouseretail.com/MobileTraining/Documents/Download/',
+        // baseDocumentWithoutRedirectUrl: 'http://fmsync.intelligentdata.com/MobileTraining/Documents/DownloadWithoutRedirect/',
+        // baseThumbnailDocumentUrl: 'https://fmportal.powerhouseretail.com/MobileTraining/Documents/Thumbnail/',
+        // databaseName: 'FMSync.DB',
+        // databaseVersion: '1.0'
 
         //SVW training
         // baseApiUrl: 'https://thevault.svwinc.com/MobileTraining/api/',
